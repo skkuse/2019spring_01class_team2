bdd473d0a69df9d2c5e93b48eadd4aa59b3ed0cb
young <youngerous@gmail.com>
2019-05-05 00:58:39 +0900

    회원가입 기능 테스트 작성

--- src/chwimi/account/tests.py
+++ src/chwimi/account/tests.py
@@ -1,4 +1,9 @@
 from django.test import TestCase
+from django.urls import resolve
+from account.views import signup
 
 class SignupTest(TestCase):
-    pass
+    def test_signup_url_resolves_to_signup_page_view(self):
+        found = resolve('/account/signup/')
+        self.assertEqual(found.func, signup)
+        
