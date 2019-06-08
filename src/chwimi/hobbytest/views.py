2a30c642fd4b554ddfe267c3f1567813f9ed1538
young <youngerous@gmail.com>
2019-06-08 21:23:43 +0900

    freeze req

--- src/chwimi/hobbytest/views.py
+++ src/chwimi/hobbytest/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render, redirect
+import json
+import random
+
+from django.shortcuts import redirect, render
+from recombee_api_client.api_client import RecombeeClient
+from recombee_api_client.api_requests import (AddItemProperty, AddPurchase,
+                                              AddRating, AddUserProperty,
+                                              Batch, DeleteItem,
+                                              GetItemPropertyInfo,
+                                              GetItemValues,
+                                              GetUserPropertyInfo,
+                                              GetUserValues, ListUsers,
+                                              RecommendItemsToItem,
+                                              RecommendItemsToUser,
+                                              RecommendUsersToItem,
+                                              ResetDatabase, SetItemValues,
+                                              SetUserValues)
+
 from .models import Hobby
 
 # Create your views here.
@@ -82,7 +99,7 @@ def test_4(request):
             return redirect('../test_4_1')
         hobby.test_4 = ans_4
         hobby.save()
-        return redirect('../test5')
+        return redirect('../test_5')
     return render(request, 'test_4.html')
 
 def test_4_1(request):
@@ -123,7 +140,109 @@ def test_5_1(request):
     return render(request, 'test_5_1.html')
 
 def result(request):
-    # Recombee 함수 삽입
     # 객체 변수 값이 -1인 경우 객체 삭제
     hobby = get_user_hobby(request.user)
-    return render(request, 'result.html', {'hobby':hobby})
+    category, item = recommend(hobby)
+    item = item[1]
+
+    return render(request, 'result.html', {'hobby':hobby, 'item':item, 'category':category})
+
+def recommend(hobby):
+    client = RecombeeClient('skku-testinguser', 'L1zVNlMZfzgizO0C0bqB3DA87xEpSmH01DdXos7ZyZoJHzvsyuwDAtOu5bLZqyIs')
+    
+    user_O = hobby.test_2 # Openness          (외향성 - 오락형)
+    user_A = hobby.test_3 # Agreeableness     (친화성 - 감성형)
+    user_E = hobby.test_4 # Extraversion      (개방성 - 창작형)
+    user_C = hobby.test_5 # Conscientiousness (성실성 - 제작형)
+    account = hobby.user
+    
+    client.send(SetUserValues(account,
+        {
+            "Openness": user_O,
+            "Agreeableness": user_A,
+            "Extraversion": user_E,
+            "Conscientiousness": user_C,
+            "Result": [],
+            "recommenditems": [],
+        },
+        cascade_create = True))
+
+    # filtering
+    result = []
+    if((user_A >= user_C) and (user_A >= user_O) and (user_A >= user_E)):
+        result =  ["Gamsung"]
+        if(user_A == user_C):
+            result.append("Make")
+        if(user_A == user_O):
+            result.append("Fun")
+        if(user_A == user_E):
+            result.append("Create")
+    elif((user_C >= user_A) and (user_C >= user_O) and (user_C >= user_E)):
+        result =  ["Make"]
+        if(user_C == user_A):
+            result.append("Gamsung")
+        if(user_C == user_O):
+            result.append("Fun")
+        if(user_C == user_E):
+            result.append("Create")
+    elif((user_O >= user_A) and (user_O >= user_C) and (user_O >= user_E)):
+        result =  ["Fun"]
+        if(user_O == user_A):
+            result.append("Gamsung")
+        if(user_O == user_C):
+            result.append("Make")
+        if(user_O == user_E):
+            result.append("Create")
+    else:
+        result =  ["Create"]
+        if(user_E == user_A):
+            result.append("Gamsung")
+        if(user_E == user_C):
+            result.append("Make")
+        if(user_E == user_O):
+            result.append("Fun")
+
+    client.send(SetUserValues(account,
+    {
+        "Result": result,
+    },
+    cascade_create = True))
+    recommendation = client.send(RecommendItemsToUser (account, 3,
+        filter = "'categories' in context_user[\"Result\"]"
+                        ))
+
+    # print(recommendation)
+    recommitems = recommendation['recomms']
+    items = []
+    for j in range(len(recommitems)):
+        temp_num = recommitems[j]['id']
+        temp = client.send(GetItemValues(str(temp_num)))
+        items.append(temp['title'])
+    user = client.send(GetUserValues(account))
+    recommended = user['recommenditems']
+    recommended.append(random.choice(items))
+    client.send(SetUserValues(account,
+        {
+            "recommenditems": recommended,
+        },
+        cascade_create = True))
+
+    user_current = client.send(GetUserValues(account))
+    user_item = user_current['recommenditems']
+    user_type = user_current['Result']
+    # print(user_item)
+    # print(user_type)
+
+    give = [account,user_item[0]]
+
+    for idx in range(len(user_type)):
+        if user_type[idx] == 'Make':
+            user_type[idx] = '제작형'
+        elif user_type[idx] == 'Create':
+            user_type[idx] = '창작형'
+        elif user_type[idx] == 'Fun':    
+            user_type[idx] = '오락형'
+        elif user_type[idx] == 'Gamsung':
+            user_type[idx] = '감성형'
+        
+    return user_type, give
